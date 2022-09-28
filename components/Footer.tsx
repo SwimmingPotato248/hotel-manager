4628509fa16b7a9d9923f19173edaa8c91536deb
SwimmingPotato <ptphong200i@gmail.com>
2022-09-28 09:14:00 +0700

    Add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="mt-auto bg-black text-white text-center py-2">
+      <Link href="https://github.com/SwimmingPotato248/hotel-manager">
+        Github
+      </Link>
+    </div>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 }
